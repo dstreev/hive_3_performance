c2dedcba5a2d41c53b220f070275e48391a7e4de
David W. Streever <david@streever.com>
2022-02-09 08:45:45 -0500

    .

--- merge_files/merge_files_parse_results.sh
+++ merge_files/merge_files_parse_results.sh
@@ -22,7 +22,7 @@ search_list=("dynamic\spartitions" \
 
 FULL_SEARCH=
 for i in "${search_list[@]}"; do
-  if [ "${FULL_SEARCH}x" == "x"]; then
+  if [ "${FULL_SEARCH}x" == "x" ]; then
     FULL_SEARCH="${i}"
   else
     FULL_SEARCH="${FULL_SEARCH}\|${i}"
