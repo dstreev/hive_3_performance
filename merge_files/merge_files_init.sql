5302a987223dd9657e38666b371898dd9ccb8468
David W. Streever <david@streever.com>
2022-02-09 07:35:17 -0500

    fixed init script

--- merge_files/merge_files_init.sql
+++ merge_files/merge_files_init.sql
@@ -4,6 +4,6 @@ set hive.optimize.sort.dynamic.partition=${dynamic};
 set hive.optimize.sort.dynamic.partition.threshold=${dynamic_threshold};
 
 set hive.merge.tezfiles=false;
-set mapred.reduce.tasks=false;
+set mapred.reduce.tasks=-1;
 set hive.stats.autogather=${gather_stats};
 set hive.stats.column.autogather=${gather_stats};
