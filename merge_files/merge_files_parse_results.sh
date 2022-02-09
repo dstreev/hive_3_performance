1b041aced39c53e1496cd105083dd0d426bb3a43
David W. Streever <david@streever.com>
2022-02-09 08:46:28 -0500

    .

--- merge_files/merge_files_parse_results.sh
+++ merge_files/merge_files_parse_results.sh
@@ -29,7 +29,7 @@ for i in "${search_list[@]}"; do
   fi
 done
 
-echo "${FULL_SEARCH}"
+echo "${FULL_SEARCH} on $1"
 
 grep '${FULL_SEARCH}' $1
 # grep 'dynamic\spartitions\|Executing\son\sYARN\scluster\swith\sApp\sid\|^START\|^STOP\|^Counts\|[0..9]\srows\saffected\|Completed\sexecuting\scommand\|Compile\sQuery\|Prepare\sPlan\|Submit\sPlan\|Start\sDAG\|Coordinator\|Run\sDAG\|CREATED_DYNAMIC_PARTITIONS\|CREATED_FILES\|CPU_MILLISECONDS\|SPILLED_RECORDS\|TOTAL_LAUNCHED_TASKS\|GC_TIME_MILLIS' $1
