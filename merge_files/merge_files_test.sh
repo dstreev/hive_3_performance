b8a0afe945a5e6e1573dbc0c8cfc1a3a77e96c9d
David W. Streever <david@streever.com>
2022-02-09 06:26:35 -0500

    script fixes

--- merge_files/merge_files_test.sh
+++ merge_files/merge_files_test.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env sh
 
-TARGET_FILE=$HOME/merge_files_test.txt
+TARGET_FILE=$HOME/merge_files_`date +%y%m%d_%H%M`.txt
+echo "Check output file: ${TARGET_FILE}"
 
 # Setup
 echo "Running merge_files Setup" > $TARGET_FILE
-hive --hivevar db=merge_files -f merge_files_setup.sql >> $TARGET_FILE
+hive --hivevar db=merge_files -f merge_files_setup.sql >> $TARGET_FILE 2>&1
 echo "Setup complete" >> $TARGET_FILE
 
 for i in simple mti; do
@@ -14,16 +15,12 @@ for i in simple mti; do
                 if [ $d == "true" ]; then
                     for thres in -1 0 1; do
                         echo "START >> ${i} ${dist} ${stats} ${d} ${thres}" >> "${TARGET_FILE}"
-                        hive --hivevar db=merge_files --hivevar dynamic=$d --hivevar dynamic_threshold=$thres \
-                            --hivevar gather_stats=false --hivevar version=$stats -i merge_files_init.sql \
-                            -f merge_files_$i_$dist.sql >> $TARGET_FILE
+                        hive --hivevar db=merge_files --hivevar dynamic=$d --hivevar dynamic_threshold=$thres --hivevar gather_stats=false --hivevar version=$stats -i merge_files_init.sql -f merge_files_$i_$dist.sql >> $TARGET_FILE 2>&1
                         echo "STOP >> ${i} ${dist} ${stats} ${d} ${thres}" >> "${TARGET_FILE}"
                     done
                 else
                     echo "START >> ${i} ${dist} ${stats} ${d}" >> "${TARGET_FILE}"
-                    hive --hivevar db=merge_files --hivevar dynamic=$d --hivevar dynamic_threshold=-1 \
-                        --hivevar gather_stats=false --hivevar version=$stats -i merge_files_init.sql \
-                        -f merge_files_$i_$dist.sql >> $TARGET_FILE
+                    hive --hivevar db=merge_files --hivevar dynamic=$d --hivevar dynamic_threshold=-1 --hivevar gather_stats=false --hivevar version=$stats -i merge_files_init.sql -f merge_files_$i_$dist.sql >> $TARGET_FILE 2>&1
                     echo "STOP >> ${i} ${dist} ${stats} ${d}" >> "${TARGET_FILE}"
                 fi
             done
